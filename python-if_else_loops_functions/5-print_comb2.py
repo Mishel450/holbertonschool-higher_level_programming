8de60ebc66ebc5ff0ef9ce4c787288d4de3356eb
Mishel450 <tomasmishel18@gmai.com>
2023-05-29 11:24:07 -0700

    python-if_else_loops_functions

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 txt = "{}"
 for i in range(0, 100):
+    n = i
+    if (n < 10):
+        n = f"0{n}"
     if (i != 99):
-        print(txt.format(i), end=", ")
+        print(txt.format(n), end=", ")
     else:
-        print(txt.format(i))
+        print(txt.format(n))
