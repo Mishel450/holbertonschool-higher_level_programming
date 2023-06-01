7bfea42079dcc57f956d5503d2c4a6f0a21e8cd9
Mishel450 <tomasmishel18@gmai.com>
2023-06-01 10:52:07 -0700

    import_modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    txt = "{}"
     import hidden_4
-    a = (dir(hidden_4)) 
+    a = (dir(hidden_4))
     b = (dir(hidden_4))
     for i in a:
         if (i[0] == "_"):
             b.remove(i)
-    print(b)
+    for j in b:
+        if (j != 0):
+            print(txt.format(j))
