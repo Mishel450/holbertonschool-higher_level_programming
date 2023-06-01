be12478dbd178213335287cdfcbab5a06e81c3a5
Mishel450 <tomasmishel18@gmai.com>
2023-06-01 09:47:02 -0700

    import_modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    txtarguments = "{} {}:"
+    txtarguments = "{} {}{}"
     txt = "{}: {}"
     if (len(argv) - 1 == 1):
-        print(txtarguments.format(len(argv) - 1, "argument"))
+        print(txtarguments.format(len(argv) - 1, "argument", ":"))
+    elif (len(argv) - 1 == 0):
+        print(txtarguments.format(len(argv) - 1, "arguments", "."))
     else:
-        print(txtarguments.format(len(argv) - 1, "arguments"))
+        print(txtarguments.format(len(argv) - 1, "arguments", ":"))
     for i in range(1, len(argv)):
         print(txt.format(i, argv[i]))
