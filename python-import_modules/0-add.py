9d590d648cdc324aafa7a4602a8e9d53a40f7c57
Mishel450 <tomasmishel18@gmai.com>
2023-06-01 07:11:13 -0700

    import_modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-txt = "{} + {} = {}"
-a = 1
-b = 2
-c = add(a, b)
-print(txt.format(a, b, c))
+if __name__ == "__main__":
+    from add_0 import add
+    txt = "{} + {} = {}"
+    a = 1
+    b = 2
+    c = add(a, b)
+    print(txt.format(a, b, c))
