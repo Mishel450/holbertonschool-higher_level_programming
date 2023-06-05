95452663212b3c2313a0ee863a3ebece3e552735
Mishel450 <tomasmishel18@gmai.com>
2023-06-05 10:50:17 -0700

    data_structure

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    size = len(sentence)
-    if (sentence[0] is not None):
-        fchr = sentence[0]
+    if (len(sentence) is not None):
+        size = len(sentence)
+        if (sentence[0] is not None):
+            fchr = sentence[0]
+        return (size, fchr)
     else:
-        fchr = None
-    return (size, fchr)
+        return (len(sentence), "None")
