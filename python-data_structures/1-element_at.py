8bb9be2c07370032b9c4f5ecea2ae7d5e3ce5381
Mishel450 <tomasmishel18@gmai.com>
2023-06-05 05:51:37 -0700

    data_structure

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if (idx > 0 or idx > len(my_list)):
-        count = 0
-        for i in range(len(my_list)):
-            if (count == idx):
-                return (my_list[i])
-            count = count + 1
-            if (count == len(my_list)):
-                return ("None")
-    else:
-        return ("None")
+    if (idx > 0 and idx < len(my_list)):
+        return (my_list[idx])
