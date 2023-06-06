9b10a89ed6c7f5580899948210530acd739b4164
Mishel450 <tomasmishel18@gmai.com>
2023-06-06 05:17:19 -0700

    data_structures

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    listt = [0] * len(my_list)
+    for i in range (len(my_list)):
+        if (my_list[i] % 2 == 0):
+            listt[i] = 1 <= 2
+        else:
+            listt[i] = 0 == 1
+    return (listt)
