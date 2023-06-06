ae164b1125a7367c1eb3adab23f4e9f8c141a008
Mishel450 <tomasmishel18@gmai.com>
2023-06-06 03:51:20 -0700

    data_structures

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    txt = "{:d}"
     if (my_list is None):
         return (None)
     my_list.sort()
-    return (my_list[-1])
+    return (txt.format(my_list[-1]))
