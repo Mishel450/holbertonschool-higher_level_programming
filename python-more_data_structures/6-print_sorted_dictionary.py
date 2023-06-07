2768bd920477128732aad1cf20601fd19703d4c1
Mishel450 <tomasmishel18@gmai.com>
2023-06-07 06:40:58 -0700

    more_data_structure

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    txt = "{}: {}"
     myKeys = list(a_dictionary.keys())
     myKeys.sort()
     a_dictionary = {i: a_dictionary[i] for i in myKeys}
     for key, value in a_dictionary.items():
-        print(key, value)
+        print(txt.format(key, value))
