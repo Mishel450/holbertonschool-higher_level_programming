1833971dd993db2994c503f585c04218581e1d10
Mishel450 <tomasmishel18@gmai.com>
2023-06-14 05:15:23 -0700

    python-classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,8 +2,10 @@
 """python3 -c 'print(__import__("my_module").__doc__)'
 module
 """
+
+
 class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'
-    Square 
+    Square
     """
     pass
