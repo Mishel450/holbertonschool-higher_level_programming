d27354a3734c353ef1c05b6148e891376b3485fd
Mishel450 <tomasmishel18@gmai.com>
2023-06-15 06:29:52 -0700

    python classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'
     Square
     """
-    
+
     def __init__(self, __size=0):
         """python3 -c 'print(__import__("my_module").__doc__)'
         init
@@ -22,7 +22,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -30,7 +30,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         return (self.__size * self.__size)
-
