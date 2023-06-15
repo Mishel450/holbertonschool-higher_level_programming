3a6badfe3304ffa5608ec024b28c28bb35094eee
Mishel450 <tomasmishel18@gmai.com>
2023-06-15 10:57:47 -0700

    test driven developmen

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,9 +5,11 @@
 def say_my_name(first_name, last_name=""):
     """say_my_name"""
 
-    txt = "My name is {} {}"
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print(txt.format(first_name, last_name))
+    if (last_name != ""):
+        print("My name is", first_name, last_name)
+    else:
+        print("My name is", first_name)
