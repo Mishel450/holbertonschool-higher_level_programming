b11bf32b938c16e6d00f6c8cb2cf6721b1c1bd21
Mishel450 <tomasmishel18@gmai.com>
2023-06-15 06:48:37 -0700

    python classes

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'
+module
+"""
+
+
+class Square:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+    Square
+    """
+
+    def __init__(self, __size=0):
+        """python3 -c 'print(__import__("my_module").__doc__)'
+        init
+        """
+        if not isinstance(__size, int):
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = __size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if (self.__size > 0):
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        elif (self.__size == 0):
+            print()
