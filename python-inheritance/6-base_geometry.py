6513da9cc6dbf1f299520332abcd567d1ea57d03
Mishel450 <tomasmishel18@gmai.com>
2023-06-20 10:25:12 -0700

    inheritane

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""5-task"""
+
+
+class BaseGeometry:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+    pass
+
+    def area(self):
+        if isinstance (self == None):
+            raise Exception("area() is not implemented")
