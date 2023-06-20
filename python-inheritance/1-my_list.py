8ebb0b22570d66f67ee25482384098a900424680
Mishel450 <tomasmishel18@gmai.com>
2023-06-20 07:25:23 -0700

    inheritane

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,8 +5,9 @@
 class MyList(list):
     """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
 
-
+    
     def print_sorted(self):
         """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        list.sort
-        print(list)
+        a = self.copy()
+        a.sort()
+        print(a)
