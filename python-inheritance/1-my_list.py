51961d3332ac6458af93cb9307a9b48429606794
Mishel450 <tomasmishel18@gmai.com>
2023-06-20 06:55:58 -0700

    inheritance

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""1-test"""
+
+
+class MyList(list):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+
+    def print_sorted(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+        list.sort
+        print(list)
