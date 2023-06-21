3221cf1593fe84b6b5ff4e5aae124b27ce67bcf6
Mishel450 <tomasmishel18@gmai.com>
2023-06-21 07:54:42 -0700

    input_output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""0-task"""
+
+
 def read_file(filename=""):
+    """hola"""
+
     with open(filename, encoding="utf-8") as file:
         file_read = file.read()
-        return(print((file_read), end=""))
+        print(file_read, end="")
