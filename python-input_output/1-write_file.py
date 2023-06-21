170739b7c9e6c5f4e81a6b71900cee2ed33167ee
Mishel450 <tomasmishel18@gmai.com>
2023-06-21 09:34:38 -0700

    input_output

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """write_file"""
 
-    with open(filename, 'r+', encoding="utf-8") as file:
+    with open(filename, 'w', encoding="utf-8") as file:
         return (file.write(text))
