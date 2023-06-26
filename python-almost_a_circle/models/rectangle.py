cc4377206057fdee744025eef3804a182c5d74e0
Mishel450 <tomasmishel18@gmai.com>
2023-06-26 05:58:42 -0700

    almost a circle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""2-task"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
+
+    @property
+    def width(self):
+        return self.width
+
+    @property
+    def height(self):
+        return self.height
+
+    @property
+    def x(self):
+        return self.x
+
+    @property
+    def y(self):
+        return self.y
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__width = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+
+        bs = Base(id)
+        self.id = bs.id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
