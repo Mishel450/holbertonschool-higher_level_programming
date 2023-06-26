73165ba89553192be6ba9592da30f732775177ea
Mishel450 <tomasmishel18@gmai.com>
2023-06-26 06:40:33 -0700

    almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,29 +18,29 @@ class Rectangle(Base):
     def width(self):
         return self.width
 
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
     @property
     def height(self):
         return self.height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
 
     @property
     def x(self):
         return self.x
 
-    @property
-    def y(self):
-        return self.y
-
-    @width.setter
-    def width(self, value):
-        self.__width = value
-
-    @height.setter
-    def height(self, value):
-        self.__width = value
-
     @x.setter
     def x(self, value):
         self.__x = value
+
+    @property
+    def y(self):
+        return self.y
 
     @y.setter
     def y(self, value):
