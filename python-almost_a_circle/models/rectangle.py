33eb6727df61b1862c66f43dbf221a626d82888d
Mishel450 <tomasmishel18@gmai.com>
2023-06-27 07:14:27 -0700

    almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -104,3 +104,12 @@ class Rectangle(Base):
                     self.x = value
                 if key == "y":
                     self.y = value
+    
+    def to_dictionary(self):
+        """dict function"""
+        d1 = dict(x=self.__x,
+                  y=self.__y,
+                  id=self.id,
+                  height=self.__height,
+                  width=self.width)
+        return (d1)
