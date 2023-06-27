755bdfbff261508796b2daa8250e810f450f9718
Mishel450 <tomasmishel18@gmai.com>
2023-06-27 06:33:16 -0700

    almost a circle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,10 +8,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        return ("[Square] ({}) {}/{} - {}".format
-                (self.id, self.x, self.y, self.width))
-
     @property
     def size(self):
         return self.width
@@ -19,3 +15,7 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         self.update(width=value, height=value)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format
+                (self.id, self.x, self.y, self.width))
