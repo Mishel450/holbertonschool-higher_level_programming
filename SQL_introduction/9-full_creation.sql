bf42d4087643093a3b789ea272b74ca2b9bcbe2f
Mishel450 <tomasmishel18@gmai.com>
2023-07-25 07:16:17 -0700

    9-full_creation.sql 1.2

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE if not EXISTS second_table (
     name varchar(256),
     score int
 )
-INSERT INTO second_table (id, name, score) VALUE (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUE (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUE (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUE (4, 'George', 8);
+INSERT INTO second_table(id, name, score) VALUE (1, 'John', 10);
+INSERT INTO second_table(id, name, score) VALUE (2, 'Alex', 3);
+INSERT INTO second_table(id, name, score) VALUE (3, 'Bob', 14);
+INSERT INTO second_table(id, name, score) VALUE (4, 'George', 8);
