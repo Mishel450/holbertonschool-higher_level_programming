a9a5957284674e3ca5754ab3dfd53ca67d8da161
Mishel450 <tomasmishel18@gmai.com>
2023-07-26 09:21:55 -0700

    2-create_read_user.sql done

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- task-2
+
+CREATE DATABASE if not EXISTS hbtn_0d_2;
+CREATE USER if not EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT on *hbtn_0d_2* TO 'user_0d_2'@'localhost';
