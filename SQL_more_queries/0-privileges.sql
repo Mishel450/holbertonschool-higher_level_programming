4ae7588761793e75e7d1553b1ea06f8ce90d4743
Mishel450 <tomasmishel18@gmai.com>
2023-07-26 08:57:31 -0700

    0-privileges.sql 1.2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- task-0
 
-GRANT ALL on *.* TO 'user_0d_1'@'localhost';
-GRANT ALL on *.* TO 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
