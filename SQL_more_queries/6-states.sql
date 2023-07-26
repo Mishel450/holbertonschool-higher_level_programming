ed0389020b6431a1f93157387371dce051579e98
Mishel450 <tomasmishel18@gmai.com>
2023-07-26 09:50:56 -0700

    6-states.sql done

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,7 +2,6 @@
 
 CREATE DATABASE if not EXISTS hbtn_0d_usa;
 CREATE TABLE states (
-    id int UNIQUE AUTO_INCREMENT NOT NULL,
+    id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name varchar(256)
-    PRIMARY KEY (id)
 );
