8b906668bf8ef95cfacb3ffebb725aec8478f509
Mishel450 <tomasmishel18@gmai.com>
2023-07-26 09:51:57 -0700

    6-states.sql 1.1

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- task-6
 
 CREATE DATABASE if not EXISTS hbtn_0d_usa;
-CREATE TABLE states (
+CREATE TABLE if not EXISTS states (
     id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name varchar(256)
 );
