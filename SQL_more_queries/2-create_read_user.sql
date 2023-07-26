5c7ec76a39c73d9b0535e2ea113338ca54425591
Mishel450 <tomasmishel18@gmai.com>
2023-07-26 09:26:54 -0700

    2-create_read_user.sql 1.1

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 
 CREATE DATABASE if not EXISTS hbtn_0d_2;
 CREATE USER if not EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT on `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
