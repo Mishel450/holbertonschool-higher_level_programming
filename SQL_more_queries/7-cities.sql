337b8ef5ba3c4ea7e9ebcb141bbd62b2c3ba9c71
Mishel450 <tomasmishel18@gmai.com>
2023-07-26 10:03:56 -0700

    7-cities.sql DONE

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- task-7
+
+CREATE DATABASE if not EXISTS hbtn_0d_usa;
+CREATE TABLE if not EXISTS hbtn_0d_usa.cities (
+    id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    state_id int NOT NULL FOREIGN KEY (id) REFERENCES states(id),
+    name varchar(256) NOT NULL
+);
