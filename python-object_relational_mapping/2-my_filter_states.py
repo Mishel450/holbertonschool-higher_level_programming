e2a8d51f58d383fd7df40afd989622466adde906
Mishel450 <tomasmishel18@gmai.com>
2023-07-27 09:16:31 -0700

    2-my_filter_states.py done

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""task-2"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_conection = MySQLdb.connect(host='localhost', database=argv[3],
+                                   user=argv[1], password=argv[2])
+    cursor = db_conection.cursor()
+    mySql_select_states = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(mySql_select_states)
+    for i in cursor.fetchall():
+        if i[1] == argv[4]:
+            print(i)
+    db_conection.close()
