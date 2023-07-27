58f467e05978e5ba76e8c1b886f8135a71e83445
Mishel450 <tomasmishel18@gmai.com>
2023-07-27 10:54:45 -0700

    3-my_safe_filter_states.py 1.1

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     cursor = db_conection.cursor()
     mySql_select_states = "SELECT * FROM states ORDER BY id ASC"
     cursor.execute(mySql_select_states)
-    querty = argv[4]
+    query = argv[4]
     for i in cursor.fetchall():
-        if i[1] == querty:
+        if i[1] == query:
             print(txt.format(i))
     db_conection.close()
