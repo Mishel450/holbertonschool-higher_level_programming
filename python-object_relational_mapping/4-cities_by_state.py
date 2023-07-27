b98a3ef88728ca3da29653d730e251262c2391c1
Mishel450 <tomasmishel18@gmai.com>
2023-07-27 12:53:31 -0700

    4-cities_by_state.py done

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,9 +8,8 @@ if __name__ == "__main__":
     db_conection = MySQLdb.connect(host='localhost', database=argv[3],
                                    user=argv[1], password=argv[2])
     cursor = db_conection.cursor()
-
-    cursor.execute("SELECT * FROM cities ORDER BY id ASC")
-
+    mySql_select_states = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(mySql_select_states)
     for i in cursor.fetchall():
         print(i)
     db_conection.close()
