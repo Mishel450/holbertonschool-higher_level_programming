cc9e44a1728a1352015051a1392329296c7a1a99
Mishel450 <tomasmishel18@gmai.com>
2023-07-31 08:08:24 -0700

    pycodestyle model_state.py and 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,8 +6,8 @@ from sqlalchemy import create_engine
 from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                            format(argv[1], argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
 
     connect = engine.connect()
     states = sqlalchemy.select(State.id, State.name).order_by(State.id)
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,9 +6,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Class State
     """
     __tablename__ = 'states'
-    id = Column(Integer,autoincrement=True, unique=True, nullable=False, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
