f338d6eb275dae909992839eb2765c3765d74032
Mishel450 <tomasmishel18@gmai.com>
2023-07-31 07:27:51 -0700

    7-model_state_fetch_all.py done

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""7-task"""
+from sys import argv
+import sqlalchemy
+from sqlalchemy import create_engine
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                            format(argv[1], argv[2], argv[3]))
+
+    connect = engine.connect()
+    states = sqlalchemy.select(State.id, State.name).order_by(State.id)
+    res = connect.execute(states).fetchall
+
+    for states in res:
+        print("{}: {}".format(states[0], states[1]))
